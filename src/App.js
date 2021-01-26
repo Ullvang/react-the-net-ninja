0fd88ddc5fbd12009bcfdaba9c304299ed797f16
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-01-26 20:29:06 +0100

    episode 5

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "/";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{10}</p>
-        <p>{"Hello"}</p>
-        <p>{[1, 2, 3, 4, 5, "test"]}</p>
-        <p>{Math.random() * 10 ** 17}</p>
-
-        <a href={link}>Link</a>
+        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
