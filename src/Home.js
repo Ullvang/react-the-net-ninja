aa312204b713ff0b5f117c9826c0f50aefe58f5e
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-01-28 18:19:00 +0100

    #7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log(e.target);
+    console.log("Hello ninjas");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("Hello " + name);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("yoshi")}>Click me again</button>
     </div>
   );
 };
