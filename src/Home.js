bcf50008529fb8fc3c3f349551bd3993b644a8c6
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-01-28 19:34:43 +0100

    #14

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,11 @@ const Home = () => {
       id: 3,
     },
   ]);
+
+  useEffect(() => {
+    console.log("useEffect ran");
+    console.log(blogs);
+  });
 
   const handleDelete = (id) => {
     setBlogs(blogs.filter((blog) => blog.id !== id));
