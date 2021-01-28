c7873140dfde0076665540765cca2549bb964820
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-01-28 22:04:23 +0100

    #18

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,9 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(blogs);
+  console.log(isLoading);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
@@ -11,11 +14,13 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title={"All blogs"} />}
     </div>
   );
