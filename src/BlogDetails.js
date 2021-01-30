e9d4431aebac4399263e4b29d6376b1362eb606a
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-01-30 12:50:54 +0100

    #31

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -7,6 +7,16 @@ const BlogDetails = () => {
   const { data: blog, error, isLoading } = useFetch(
     "http://localhost:8000/blogs/" + id
   );
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -17,6 +27,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
